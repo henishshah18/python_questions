47e14ab40ead3280da2fa526265f171139a786cf
Henish M Shah <74452754+henishshah18@users.noreply.github.com>
2025-07-22 17:21:47 +0530

    Create personal_information_collector.py

---
+++ personal_information_collector.py
@@ -0,0 +1,9 @@
+name = input('Enter your full name: ')
+age = int(input('Enter your age: '))
+city = input('Enter your city: ')
+hobby = input('Enter your hobby: ')
+
+print('\n')
+
+print(f'Hello, {name}!\nYou are {age} years old and live in {city}.\nIn your free time, you enjoy {hobby}.')
+
